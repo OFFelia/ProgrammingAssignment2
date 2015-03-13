7317573a585413d14cdab7d2af6d9786ad0c8c83
OFFelia <yedaskal@gmail.com>
2015-03-13 10:33:28 +0100

    HW2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Process of inverse matrix calculation is usualy time-consuming. This pair of 
+## the functions helps us to cache the inverse of the matrix.
 
-## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
+## this function creates an object "matrix" that can cache the inverse matrix
+
+makeCacheMatrix <- function(x = matrix()) { ## initialization of function
+    s <- NULL                           ## initializing a local variable to NULL
+    set <- function(y) {                
+        x <<- y                         ## setting the cached matrix "x"
+        s <<- NULL                      ## setting the cached inverse to NULL
+    }
+    get <- function(){                  ## uses "x" from the cache
+       return (x)
+    }
+    setsolve <- function(solve){       ## uses inverse matrix and set it to cache 
+        s <<- solve
+    }
+    getsolve <- function(){             ## returns the cached inverse matrix
+        return (s)
+    }
+    list(set = set, get = get, ## assign values to arguments for the next function  
+         setsolve = setsolve,       
+         getsolve = getsolve)
 
 }
 
 
-## Write a short comment describing this function
+## this function calculates the inverse of special "matrix" from the previous
+## function. If the inverse has already been calculated and "matrix" is the same,
+## function just return cached inverse.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cachesolve <- function(makeCacheMatrix, ...) { ## initialization of the function
+    
+    local_solve <- makeCacheMatrix$getsolve() ## sets the local variable to cached
+    
+    if(!is.null(local_solve)) {         ## checks whether "local_solve" isn't NULL
+        message("getting cached data") ## prints message 
+        return(local_solve)            ## returns inverse from the cache. Exit
+    }
+    else {                              ## if "locale_solve is NULL
+        local_data <- makeCacheMatrix$get() ## obtains matrix from makeCacheMatrix 
+        local_solve <- solve(local_data, ...) ## calculates inverse
+        
+        makeCacheMatrix$setsolve(local_solve)  
+        return(local_solve)         ## Return a matrix that is the inverse of 'x'
+    }
 }
+        
+
+
